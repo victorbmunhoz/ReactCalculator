c08bd0dd840e7ac111a25fda8f47875443b200b0
Victor Munhoz <vbm2906@gmail.com>
2021-04-20 14:55:13 -0300

    Adicionando as funções e estados

--- react-calculator/src/components/Button.jsx
+++ react-calculator/src/components/Button.jsx
@@ -1,5 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Button.css'
 
-export default props => 
-    <button className='button'>{props.label}</button>
+export default props =>
+    <button
+        onClick={e => props.click && props.click(props.label)}
+
+
+        className={`
+            button
+            ${props.operation ? 'operation' : ''}
+            ${props.double ? 'double' : ''}
+            ${props.triple ? 'triple' : ''}
+         `}
+    >
+        {props.label}
+    </button>
